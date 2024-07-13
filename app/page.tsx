dca383c2c5a581af896578ba1b2d2b99d91bed51
Kev-Villegas <Kevin.villegas1973@gmail.com>
2024-07-13 19:42:25 -0300

    feat: adding the SectionDivider n About to the main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import Hero from "./_components/Hero";
+import About from "./_components/About";
 import Header from "./_components/Header";
+import SectionDivider from "./_components/SectionDivider";
 
 export default function Home() {
   return (
-    <main className="flex min-h-[300vh] flex-col items-center px-4">
+    <main className="flex flex-col items-center justify-center px-4">
       <Header />
       <Hero />
+      <SectionDivider />
+      <About />
     </main>
   );
 }
